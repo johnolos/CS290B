cb71de80581ed32ba71d43da6b9af5a3df1bb574
melhoos <mmelhoos@gmail.com>
2015-05-25 21:48:39 -0700

    read file

--- project/src/application/longestpath/Graph.java
+++ project/src/application/longestpath/Graph.java
@@ -1,13 +1,51 @@
 package application.longestpath;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Graph {
 
-	private int[][] nodes; 
+	private static int numberOfNodes; 
 	
-	public static int[][] graphForNodes(String filename){
+	public static int[][] graphForNodes(String filename) throws FileNotFoundException{
 		
+		int nodes[][]; 
 		
+		try(Scanner in = new Scanner(new FileReader(filename))){
+			
+			numberOfNodes = in.hasNextInt() ? in.nextInt() : -1;
+			
+			if(numberOfNodes > 0) {
+				nodes = new int[numberOfNodes][];
+				
+				for (int i = 0; i < numberOfNodes; i++) {
+					nodes[i] = new int[0]; 
+				}
+				
+				while (in.hasNextLine()) {
+					String line = in.nextLine();
+					String[] stringArr = line.split(" "); 
+					int fromNode = Integer.parseInt(stringArr[0]); 
+					int toNode = Integer.parseInt(stringArr[1]);
+					int value = Integer.parseInt(stringArr[2]); 
+					int arrayLength = nodes[fromNode].length; 
+					int[] replacement = Arrays.copyOf(nodes[fromNode], arrayLength + 2);
+					replacement[arrayLength] =  toNode; 
+					replacement[arrayLength + 1] = value; 
+					nodes[fromNode] = replacement;
+				}
+				in.close();
+				return nodes; 
+			}  
+			
+		} catch(FileNotFoundException e){
+		}
+		throw new FileNotFoundException("Format wrong");
 	}
+	
+
 	
 	
 }
